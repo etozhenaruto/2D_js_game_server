275b279d394f8b98e65ad8ac592d5541b1cd1752
Ilja Rekaev <ilya.rekaev@irlix.ru>
2022-10-10 18:43:25 +0400

    characters

---
+++ src/Enties/Character.js
@@ -0,0 +1,14 @@
+class Character{
+    name
+    hp
+    damage
+    
+    constructor(name,hp,damage){
+        this.name = name
+        this.hp = hp
+        this.damage= damage
+    }
+
+
+}
+module.exports = Character
